c81bad8bce05187c2162027bae934dcfed6be313
larstorngrenchas <lars.torngren@chasacademy.se>
2026-03-11 19:43:36 +0100

    Initial Terraform setup för Lab 1

---
+++ outputs.tf
@@ -0,0 +1,12 @@
+# outputs.tf
+output "vm_name" {
+  value = google_compute_instance.vm.name
+}
+
+output "vm_external_ip" {
+  value = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
+}
+
+output "vm_zone" {
+  value = google_compute_instance.vm.zone
+}
